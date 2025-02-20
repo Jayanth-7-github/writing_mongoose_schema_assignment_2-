cbeacd244254245bc3b09441653dd44a4e38d6a8
Jayanth <kolleparajayanth@gmail.com>
2025-02-20 09:57:09 +0530

    done

---
+++ schema.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+// Comment Schema
+const commentSchema = new mongoose.Schema({
+    username: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    commentedAt: {
+      type: Date,
+      default: Date.now,
+    }
+}, { timestamps: { createdAt: 'commentedAt', updatedAt: false } });
+
+// BlogPost Schema
+const blogPostSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    unique: true,
+    minlength: 5,
+    required: true,
+  },
+  content: {
+    type: String,
+    minlength: 50,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+  },
+  category: {
+    type: String,
+    default: "General",
+  },
+  likes: {
+    type: [String],
+  }
+}, { 
+  timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } 
+});
+
+// Create Models
+const Comment = mongoose.model('Comment', commentSchema);
+const BlogPost = mongoose.model('BlogPost', blogPostSchema);
+
+// Export Models
+module.exports = { Comment, BlogPost };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
